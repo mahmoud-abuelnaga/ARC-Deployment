e4e8d44bbb1ff088c622ae043e0b9e5657460eca
Mahmoud Abuelnaga <mahmoud.abuelnaga0@gmail.com>
2026-01-15 12:36:52 +0200

    feat: Add initial configuration and GitHub App definition for Actions Runner Controller (ARC).

--- main.sh
+++ main.sh
@@ -19,13 +19,23 @@ CONTROLLER_UPDATE_STRATEGY="eventual"
 CONTROLLER_RELEASE_NAME="gha-runner-scale-set-controller"
 CONTROLLER_NAMESPACE="arc-system"
 
+# scale set configuration
+SCALE_SET_CHART_URL="oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set:0.13.1"
+SCALE_SET_CHART_DIR="gha-runner-scale-set"
+SCALE_SET_RELEASE_NAME="gha-runner-scale-set"
+SCALE_SET_NAMESPACE="arc-runners"
+
 # functions
+## cluster
 start_cluster() {
   if ! minikube status; then
     minikube start --cpus "$CPUS" --memory "$MEMORY" --cni "$CNI"
   fi
 }
 
+## github 
+
+## controller
 get_controller_values_template() {
   mkdir -p "$CONTROLLER_CHART_DIR"
   helm show values "$CONTROLLER_CHART_URL" >"$CONTROLLER_CHART_DIR/values.yaml"
@@ -53,3 +63,31 @@ install_controller() {
     --namespace "$CONTROLLER_NAMESPACE" \
     --create-namespace
 }
+
+## scale set
+get_scale_set_values_template() {
+  local number=$1
+  if [[ -z $number ]]; then
+    echo "Scale set number not provided"
+    return 1
+  fi
+
+  mkdir -p "$SCALE_SET_CHART_DIR-$number"
+  helm show values "$SCALE_SET_CHART_URL" >"$WORKDIR/$SCALE_SET_CHART_DIR-$number/values.yaml"
+}
+
+setup_pat_token() {
+  local number=$1
+  if [[ -z $number ]]; then
+    echo "Scale set number not provided"
+    return 1
+  fi
+
+  yq 'del(.githubConfigSecret.github_token)' "$WORKDIR/$SCALE_SET_CHART_DIR-$number/values.yaml"
+
+}
+
+
+
+# main
+get_scale_set_values_template "$1"
